9b89a8ba55c2fe423d299b507109517461d9ca64
Willian Pedroso <willianpedroso@prodam.sp.gov.br>
2015-11-25 15:44:11 -0200

    layout formulario identificação

--- platforms/android/bin/MainActivity-debug-unaligned.apk.d
+++ platforms/android/bin/MainActivity-debug-unaligned.apk.d
@@ -1,4 +1,4 @@
-C:\wamp\www\App_DBA\platforms\android\bin\MainActivity-debug-unaligned.apk : \
-C:\wamp\www\App_DBA\platforms\android\bin\MainActivity.ap_ \
-C:\wamp\www\App_DBA\platforms\android\bin\classes.dex \
-C:\wamp\www\App_DBA\platforms\android\CordovaLib\bin\classes.jar \
+C:\Users\p051234\Documents\Android\Projetos\App_DBA\platforms\android\bin\MainActivity-debug-unaligned.apk : \
+C:\Users\p051234\Documents\Android\Projetos\App_DBA\platforms\android\bin\MainActivity.ap_ \
+C:\Users\p051234\Documents\Android\Projetos\App_DBA\platforms\android\bin\classes.dex \
+C:\Users\p051234\Documents\Android\Projetos\App_DBA\platforms\android\CordovaLib\bin\classes.jar \
--- platforms/android/bin/classes.dex.d
+++ platforms/android/bin/classes.dex.d
@@ -1,12 +1,12 @@
-C:\wamp\www\App_DBA\platforms\android\bin\classes.dex : \
-C:\wamp\www\App_DBA\platforms\android\bin\classes\br\gov\sp\prodam\dba_app\BuildConfig.class \
-C:\wamp\www\App_DBA\platforms\android\bin\classes\br\gov\sp\prodam\dba_app\MainActivity.class \
-C:\wamp\www\App_DBA\platforms\android\bin\classes\br\gov\sp\prodam\dba_app\R$attr.class \
-C:\wamp\www\App_DBA\platforms\android\bin\classes\br\gov\sp\prodam\dba_app\R$drawable.class \
-C:\wamp\www\App_DBA\platforms\android\bin\classes\br\gov\sp\prodam\dba_app\R$string.class \
-C:\wamp\www\App_DBA\platforms\android\bin\classes\br\gov\sp\prodam\dba_app\R$xml.class \
-C:\wamp\www\App_DBA\platforms\android\bin\classes\br\gov\sp\prodam\dba_app\R.class \
-C:\wamp\www\App_DBA\platforms\android\bin\classes\org\apache\cordova\whitelist\WhitelistPlugin$1.class \
-C:\wamp\www\App_DBA\platforms\android\bin\classes\org\apache\cordova\whitelist\WhitelistPlugin$CustomConfigXmlParser.class \
-C:\wamp\www\App_DBA\platforms\android\bin\classes\org\apache\cordova\whitelist\WhitelistPlugin.class \
-C:\wamp\www\App_DBA\platforms\android\bin\dexedLibs\classes-567bacc60dc01932710b20b020e1ba5a.jar \
+C:\Users\p051234\Documents\Android\Projetos\App_DBA\platforms\android\bin\classes.dex : \
+C:\Users\p051234\Documents\Android\Projetos\App_DBA\platforms\android\bin\classes\br\gov\sp\prodam\dba_app\BuildConfig.class \
+C:\Users\p051234\Documents\Android\Projetos\App_DBA\platforms\android\bin\classes\br\gov\sp\prodam\dba_app\MainActivity.class \
+C:\Users\p051234\Documents\Android\Projetos\App_DBA\platforms\android\bin\classes\br\gov\sp\prodam\dba_app\R$attr.class \
+C:\Users\p051234\Documents\Android\Projetos\App_DBA\platforms\android\bin\classes\br\gov\sp\prodam\dba_app\R$drawable.class \
+C:\Users\p051234\Documents\Android\Projetos\App_DBA\platforms\android\bin\classes\br\gov\sp\prodam\dba_app\R$string.class \
+C:\Users\p051234\Documents\Android\Projetos\App_DBA\platforms\android\bin\classes\br\gov\sp\prodam\dba_app\R$xml.class \
+C:\Users\p051234\Documents\Android\Projetos\App_DBA\platforms\android\bin\classes\br\gov\sp\prodam\dba_app\R.class \
+C:\Users\p051234\Documents\Android\Projetos\App_DBA\platforms\android\bin\classes\org\apache\cordova\whitelist\WhitelistPlugin$1.class \
+C:\Users\p051234\Documents\Android\Projetos\App_DBA\platforms\android\bin\classes\org\apache\cordova\whitelist\WhitelistPlugin$CustomConfigXmlParser.class \
+C:\Users\p051234\Documents\Android\Projetos\App_DBA\platforms\android\bin\classes\org\apache\cordova\whitelist\WhitelistPlugin.class \
+C:\Users\p051234\Documents\Android\Projetos\App_DBA\platforms\android\bin\dexedLibs\classes-f47691a8fda7d5a3e8b516a4f503c2f4.jar \
