2f335cffa102ae35d6505adf9bf76a2f04d7cd0f
Daniel Niemietz Braz <arielceres@gmail.com>
2016-04-18 08:46:41 -0300

    Mudança de datepicker

--- www/js/funcoes.js
+++ www/js/funcoes.js
@@ -7,14 +7,15 @@ function onDeviceReady() {
     }, false );}
 
 $(document).ready(function(){
-
+/*
 $(document).on('click', '.datepicker', function () {
                 showDatePicker($(this), 'date');
                 console.log('cliquei no datepicker');
             });
+*/
 
 
-
+$( "#data_nascimento" ).datepicker();
 
  var flag = "1";
 $('#bt_aba, #bt_opt').click(function(event){
